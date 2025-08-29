f599e2ae83da8f60b4eb1fcef0f78120b1c97f60
Seth Durda <137535396+Vullarim@users.noreply.github.com>
2025-08-29 10:02:32 -0500

    Fixed veiw_tasks function

--- todo.py
+++ todo.py
@@ -2,6 +2,7 @@
 
 # Step 1: Start with an empty list to hold tasks
 tasks = []
+completed_tasks = []
 
 # Step 2: Add a task
 def add_task(task):
@@ -9,14 +10,14 @@ def add_task(task):
 
 # Step 3: View tasks
 def view_tasks():
-    print(tasks)
-    
+    for i, task in enumerate(tasks, start=1):
+        print(f"{i}. {task}")
+
 # Step 4: Delete a task
 def delete_task(task):
     tasks.pop(task)
 
 # Step 5: Mark task complete
-
 
 # Step 6: Save/load tasks (extra stretch for today)
 
@@ -26,6 +27,6 @@ if __name__ == "__main__":
     add_task("Finish Cyber 201 assignment")
     add_task("Push code to GitHub")
     view_tasks()
-    mark_complete(0)
+   # mark_complete(0)
     view_tasks()
-    save_tasks()
+  #  save_tasks()
